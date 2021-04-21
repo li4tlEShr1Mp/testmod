699c23551a550f77337d7e6949d649320fa02db7
AiR-SKY <942502856@qq.com>
2021-04-21 23:39:01 +0800

    [AiR_Sky's Edition v1.0] Added Mithril.

--- src/main/java/com/test/testmod/common/blocks/Block_Mithril.java
+++ src/main/java/com/test/testmod/common/blocks/Block_Mithril.java
@@ -8,8 +8,17 @@ import com.test.testmod.util.loaders.CreativeTabLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.Random;
 
 public class Block_Mithril extends Block implements IModelExisted {
     private static final String name="mithril";
@@ -31,8 +40,13 @@ public class Block_Mithril extends Block implements IModelExisted {
     }
 
     @Override
+    public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, ItemStack stack) {
+        super.harvestBlock(worldIn, player, pos, state, te, stack);
+    }
+
+    @Override
     public void registerModel() {
-        TestMod.proxy.registerItemRenderer(Item.getItemFromBlock(this),0,"inventory");
+        TestMod.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
 
     }
 }
